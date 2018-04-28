9352b7097b1cff61123621abe637b2bdfb747cb4
Yadickson Soto <yadickson@gmail.com>
2018-04-28 16:56:23 -0300

    Fix karma load config file

--- index.js
+++ index.js
@@ -117,8 +117,7 @@ function tasks(gulp, options) {
 
   gulp.task('karma-server', () => {
     return new karma({
-      basePath: '../..',
-      configFile: 'karma.conf.js',
+      configFile: __dirname + '/../../karma.conf.js',
       singleRun: true
     }).start();
   });
