7d54c40da116dd540732299ff06df17134792d40
Yadickson Soto <yadickson.soto@basedos.cl>
2018-04-30 10:27:08 -0300

    Add gulp help support

--- README.md
+++ README.md
@@ -10,14 +10,75 @@ Gulp Angular JS utils to build Web Application
 Simply run a install using your terminal and you're good to go!
 
 ```bash
-npm install --save gulp gulp-add-src gulp-imagemin gulp-ngdocs-components gulp-order gulp-sass gulp-ajsweb
+npm install --save gulp karma angular@~1.6.0 angular-animate@~1.6.0 gulp-add-src gulp-imagemin gulp-ngdocs-components gulp-order gulp-sass gulp-ajsweb
 ```
 
 If you're a cool kid using Yarn then simply just use yarn as you would
 
 ```bash
-yarn add gulp gulp-add-src gulp-imagemin gulp-ngdocs-components gulp-order gulp-ajsweb
+yarn add gulp karma angular@~1.6.0 angular-animate@~1.6.0 gulp-add-src gulp-imagemin gulp-ngdocs-components gulp-order gulp-sass gulp-ajsweb
 ```
+
+## How to use
+
+```JavaScript
+let addpaths = [
+  'node_modules/angular/angular.js',
+  'node_modules/angular-animate/angular-animate.js',
+  'node_modules/angular-route/angular-route.js',
+  'node_modules/bootstrap/**/bootstrap.js'
+];
+
+let addtestpaths = [
+  'node_modules/angular-mocks/angular-mocks.js'
+];
+
+let excludepaths = [
+  '!node_modules/angular/index.js',
+  '!node_modules/angular-animate/index.js',
+  '!node_modules/angular-route/index.js'
+];
+
+let addcss = [
+
+];
+
+let addscss = [
+
+];
+
+let addfonts = [
+
+];
+
+let orderBy = [
+  '**/jquery.js',
+  '**/angular.js',
+  '**/angular-*.js',
+  '**/bootstrap.js',
+  '**/bootstrap-*.js',
+  '*'
+];
+
+options = {
+  addpaths: addpaths,
+  addtestpaths: addtestpaths,
+  excludepaths: excludepaths,
+  addcss: addcss,
+  addscss: addscss,
+  addfonts: addfonts,
+  orderBy: orderBy,
+  port: 9100
+};
+
+let gulp = require("gulp-ajsweb")(require('gulp'), options);
+gulp.task('default', false, ['help'], () => {});
+```
+
+# ChangeLog
+
+ version 1.6.0 >= Support gulp help
+ version 1.5.0 <= not supported, please see how to use the new version
 
 ## Generator ajsweb
 
--- index.js
+++ index.js
@@ -12,18 +12,33 @@ const rename = require("gulp-rename");
 let dev = true;
 let minimal = false;
 let dest = 'dist';
+let port = 9100;
 
 function tasks(gulp, options) {
 
+  options = options || {};
+
+  options.addpaths = options.addpaths || [];
+  options.addtestpaths = options.addtestpaths || [];
+  options.excludepaths = options.excludepaths || [];
+  options.addcss = options.addcss || [];
+  options.addscss = options.addscss || [];
+  options.addfonts = options.addfonts || [];
+  options.orderBy = options.orderBy || [];
+
+  port = options.port || 9100;
+
   ajsweb.register(gulp, options);
 
-  gulp.task('clean', () => {
+  gulp = require('gulp-help')(gulp);
+
+  gulp.task('clean', 'Clean project', () => {
     return del(['build', 'dist', 'coverage', 'reports', '*.tgz', '*.zip', 'docs']);
   });
 
-  gulp.task('compile', ['scripts', 'styles', 'jshint', 'fonts', 'images', 'icon', 'views'], () => {});
+  gulp.task('compile', false, ['scripts', 'styles', 'jshint', 'fonts', 'images', 'icon', 'views'], () => {});
 
-  gulp.task('build', () => {
+  gulp.task('build', 'Build project for develop', () => {
     return new Promise(resolve => {
       minimal = false;
       dest = 'build';
@@ -31,7 +46,7 @@ function tasks(gulp, options) {
     });
   });
 
-  gulp.task('dist', () => {
+  gulp.task('dist', 'Dist project for production', () => {
     return new Promise(resolve => {
       minimal = true;
       dest = 'dist';
@@ -39,7 +54,7 @@ function tasks(gulp, options) {
     });
   });
 
-  gulp.task('docs', () => {
+  gulp.task('docs', false, () => {
     return new Promise(resolve => {
       minimal = true;
       dest = 'docs';
@@ -47,7 +62,7 @@ function tasks(gulp, options) {
     });
   });
 
-  gulp.task('scripts', () => {
+  gulp.task('scripts', false, () => {
     return ajsweb.buildScripts({
         dest: dest,
         minimal: minimal
@@ -55,7 +70,7 @@ function tasks(gulp, options) {
       .pipe(connect.reload());
   });
 
-  gulp.task('styles', () => {
+  gulp.task('styles', false, () => {
     return ajsweb.buildStyles({
         dest: dest,
         minimal: minimal
@@ -63,13 +78,13 @@ function tasks(gulp, options) {
       .pipe(connect.reload());
   });
 
-  gulp.task('jshint', () => {
+  gulp.task('jshint', false, () => {
     return ajsweb.appScripts()
       .pipe(jshint())
       .pipe(jshint.reporter());
   });
 
-  gulp.task('fonts', () => {
+  gulp.task('fonts', false, () => {
     return ajsweb.buildFonts({
         dest: dest,
         minimal: minimal
@@ -77,7 +92,7 @@ function tasks(gulp, options) {
       .pipe(connect.reload());
   });
 
-  gulp.task('views', () => {
+  gulp.task('views', false, () => {
     return ajsweb.buildViews({
         dest: dest,
         minimal: minimal
@@ -85,7 +100,7 @@ function tasks(gulp, options) {
       .pipe(connect.reload());
   });
 
-  gulp.task('images', () => {
+  gulp.task('images', false, () => {
     return ajsweb.buildImages({
         dest: dest,
         minimal: minimal
@@ -93,7 +108,7 @@ function tasks(gulp, options) {
       .pipe(connect.reload());
   });
 
-  gulp.task('icon', () => {
+  gulp.task('icon', false, () => {
     return ajsweb.buildIcon({
         dest: dest,
         minimal: minimal
@@ -101,28 +116,28 @@ function tasks(gulp, options) {
       .pipe(connect.reload());
   });
 
-  gulp.task('js2docs', function() {
+  gulp.task('js2docs', false, function() {
     return ajsweb.buildDocs({
       dest: dest,
       docpaths: ['vendors.js', 'app.js']
     });
   });
 
-  gulp.task('karma-cnf', () => {
+  gulp.task('karma-cnf', false, () => {
     return ajsweb.updateKarmaFile({
       configFile: 'karma.conf.js',
       dest: '.'
     });
   });
 
-  gulp.task('karma-server', () => {
+  gulp.task('karma-server', false, () => {
     return new karma({
       configFile: __dirname + '/../../karma.conf.js',
       singleRun: true
     }).start();
   });
 
-  gulp.task('testHtml', () => {
+  gulp.task('testHtml', false, () => {
     return ajsweb.buildIndexTest({
         dest: dest,
         minimal: minimal
@@ -131,29 +146,29 @@ function tasks(gulp, options) {
       .pipe(gulp.dest(dest));
   });
 
-  gulp.task('test', function() {
+  gulp.task('test', 'Run test project', function() {
     return new Promise(resolve => {
       runSequence(['karma-cnf'], ['build'], ['testHtml'], ['karma-server'], resolve);
     });
   });
 
-  gulp.task('connect', function() {
+  gulp.task('connect', false, function() {
     return connect.server({
-      port: 9100,
+      port: port,
       root: [dest],
       livereload: true
     });
   });
 
-  gulp.task('open', function() {
+  gulp.task('open', false, function() {
     return gulp.src(dest + '/index.html')
       .pipe(open({
-        uri: 'localhost:9100',
+        uri: 'localhost:' + port,
         app: 'firefox'
       }));
   });
 
-  gulp.task('watch', function() {
+  gulp.task('watch', false, function() {
     gulp.watch(ajsweb.paths.appScripts, ['scripts', 'jshint']);
     gulp.watch(ajsweb.paths.appStyles, ['styles']);
     gulp.watch(ajsweb.paths.appViews, ['views']);
@@ -162,34 +177,35 @@ function tasks(gulp, options) {
     gulp.watch(ajsweb.paths.appIcon, ['icons']);
   });
 
-  gulp.task('watchtest', function() {
+  gulp.task('watchtest', false, function() {
     gulp.watch(ajsweb.paths.appTests, ['scripts', 'jshint', 'testHtml']);
   });
 
-  gulp.task('serve', function() {
+  gulp.task('serve:build', 'Run develop application on server http://localhost:' + port, function() {
     return new Promise(resolve => {
       runSequence(['build'], ['connect'], ['watch'], ['open'], resolve);
     });
   });
 
-  gulp.task('serve:dist', function() {
+  gulp.task('serve:dist', 'Run product application on server http://localhost:' + port, function() {
     return new Promise(resolve => {
       runSequence(['dist'], ['connect'], ['open'], resolve);
     });
   });
 
-  gulp.task('serve:test', function() {
+  gulp.task('serve:test', 'Run tests on server http://localhost:' + port, function() {
     return new Promise(resolve => {
       runSequence(['karma-cnf'], ['build'], ['testHtml'], ['connect'], ['watch'], ['watchtest'], ['open'], resolve);
     });
   });
 
-  gulp.task('serve:docs', function() {
+  gulp.task('serve:docs', 'Run documentation application on server http://localhost:' + port, function() {
     return new Promise(resolve => {
       runSequence(['docs'], ['connect'], ['open'], resolve);
     });
   });
 
+  return gulp;
 }
 
 module.exports = tasks;
